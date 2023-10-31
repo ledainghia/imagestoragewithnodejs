4f393e6c82e5821d644d0574c750d9c2eac2fdcb
Lê Đại Nghĩa <nghialdse170198@fpt.edu.vn>
2023-10-31 22:10:27 +0700

    1st update

---
+++ express-app/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const cors = require("cors"); // Import thư viện cors
+const fs = require("fs"); // Import thư viện fs
+const app = express();
+const port = 3001;
+const ExcelJS = require("exceljs"); // Import thư viện exceljs
+app.use(fileUpload());
+
+// Xác định đường dẫn tuyệt đối đến thư mục 'uploads'
+const uploadDir = path.join(__dirname, "uploads");
+
+app.use(cors()); // Sử dụng middleware cors để cho phép các yêu cầu từ các nguồn khác nhau
+
+app.post("/upload", (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  const uploadedFiles = Array.isArray(req.files.file)
+    ? req.files.file
+    : [req.files.file];
+
+  uploadedFiles.forEach((file) => {
+    const uploadPath = path.join(uploadDir, file.name);
+
+    file.mv(uploadPath, (err) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+    });
+  });
+
+  res.send("Files uploaded!");
+});
+// Tạo một route mới để lấy danh sách tên các tệp trong thư mục uploads và xuất thành tệp Excel
+app.get("/exportToExcel", (req, res) => {
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet("Files");
+
+  // Lấy danh sách tên các tệp trong thư mục uploads
+  const files = fs.readdirSync(uploadDir);
+
+  // Thêm tên các tệp vào worksheet
+  files.forEach((file, index) => {
+    worksheet.addRow([file]);
+  });
+
+  // Tạo một tệp Excel và gửi nó về client
+  res.setHeader("Content-Type", "application/vnd.openxmlformats");
+  res.setHeader("Content-Disposition", "attachment; filename=files.xlsx");
+  workbook.xlsx.write(res).then(() => {
+    res.end();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ fe/src/App.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import axios from "axios";
+function App() {
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setSelectedFiles(e.target.files);
+    }
+  };
+
+  const handleUpload = () => {
+    if (selectedFiles) {
+      const formData = new FormData();
+      for (let i = 0; i < selectedFiles.length; i++) {
+        formData.append("file", selectedFiles[i]);
+      }
+
+      axios
+        .post("http://localhost:3001/upload", formData)
+        .then((response) => {
+          // Xử lý phản hồi từ server (ví dụ: hiển thị thông báo)
+          console.log(response);
+        })
+        .catch((error) => {
+          // Xử lý lỗi (ví dụ: hiển thị thông báo lỗi)
+          console.log(error);
+        });
+    }
+  };
+
+  const exportToExcel = () => {
+    axios
+      .get("http://localhost:3001/exportToExcel", {
+        responseType: "arraybuffer",
+      })
+      .then((response) => {
+        // Tạo một tệp Excel từ dữ liệu binary
+        const blob = new Blob([response.data], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "files.xlsx";
+        a.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch((error) => {
+        console.error("Error exporting to Excel:", error);
+      });
+  };
+
+  return (
+    <div>
+      <input type="file" multiple onChange={handleFileChange} />
+      <button onClick={handleUpload}>Tải lên</button>
+      {selectedFiles && (
+        <div>
+          <h2>Các tệp đã chọn:</h2>
+          <ul>
+            {Array.from(selectedFiles).map((file, index) => (
+              <li key={index}>{file.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}{" "}
+      <button onClick={() => exportToExcel()}>Export to Excel</button>
+    </div>
+  );
+}
+
+export default App;
